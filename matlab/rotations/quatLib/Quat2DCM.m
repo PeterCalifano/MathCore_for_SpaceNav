a687b39bd93f440b1c4cfa74e0c731f4aa2da44a
PeterC <petercalifano.gs@gmail.com>
2025-02-02 15:19:50 +0100

    Update naming convention

--- matlab/rotations/quatLib/Quat2DCM.m
+++ matlab/rotations/quatLib/Quat2DCM.m
@@ -1,14 +1,14 @@
-function o_dDCM = Quat2DCM(i_dQuatRot, i_bIS_VSRPplus) %#codegen
+function dDCM = Quat2DCM(dQuatRot, bIS_VSRPplus) %#codegen
 arguments
-    i_dQuatRot     (4,1) double
-    i_bIS_VSRPplus (1,1) logical = true
+    dQuatRot     (4,1) double
+    bIS_VSRPplus (1,1) logical = true
 end
 %% PROTOTYPE
-% o_dDCM = Quat2DCM(i_dQuatRot, i_bIS_VSRPplus) %#codegen
+% dDCM = Quat2DCM(dQuatRot, bIS_VSRPplus) %#codegen
 % -------------------------------------------------------------------------------------------------------------
 %% DESCRIPTION
 % Function converting attitude quaternion to DCM. Conversion occurs according to VSRP+ convention
-% (right-handed) Set i_bIS_VSRPplus = false if SVRP+ convention with scalar first is being used. 
+% (right-handed) Set bIS_VSRPplus = false if SVRP+ convention with scalar first is being used. 
 % (SV) Scalar first, Vector last
 % (P) Passive 
 % (R) Successive coordinate transformations have the unmodified quaternion chain on the Right side of
@@ -19,12 +19,12 @@ end
 % Determination and Control, Markley, Crassidis, 2014
 % -------------------------------------------------------------------------------------------------------------
 %% INPUT
-% i_dQuatRot:       [4, 1] Input quaternion to convert to DCM
-% i_bIS_VSRPplus:   [1]  Boolean flag indicating the convention of the
+% dQuatRot:       [4, 1] Input quaternion to convert to DCM
+% bIS_VSRPplus:   [1]  Boolean flag indicating the convention of the
 %                       quaternion. 1: VSRPplus, 0: SVRPplus (as MATLAB)
 % -------------------------------------------------------------------------------------------------------------
 %% OUTPUT
-% o_dDCM: [3, 3] Output rotation matrix
+% dDCM: [3, 3] Output rotation matrix
 % -------------------------------------------------------------------------------------------------------------
 %% CHANGELOG
 % 01-09-2023    Pietro Califano     Coded from reference. Validated.
@@ -38,34 +38,34 @@ end
 %% Function code
 
 % DCM initialization
-o_dDCM = coder.nullcopy(zeros(3,3));
+dDCM = coder.nullcopy(zeros(3,3));
 
 % Get quaternion components depending on quaternion order
-if i_bIS_VSRPplus == false
+if bIS_VSRPplus == false
     % SVRPplus convention (true)
-    qs  = i_dQuatRot(1);
-    qv1 = i_dQuatRot(2);
-    qv2 = i_dQuatRot(3);
-    qv3 = i_dQuatRot(4);
+    qs  = dQuatRot(1);
+    qv1 = dQuatRot(2);
+    qv2 = dQuatRot(3);
+    qv3 = dQuatRot(4);
 else
     % VSRPplus convention (false)
-    qv1 = i_dQuatRot(1);
-    qv2 = i_dQuatRot(2);
-    qv3 = i_dQuatRot(3);
-    qs  = i_dQuatRot(4);
+    qv1 = dQuatRot(1);
+    qv2 = dQuatRot(2);
+    qv3 = dQuatRot(3);
+    qs  = dQuatRot(4);
 end
 
 % Convert to DCM 
-o_dDCM(1,1) = qs^2 + qv1^2 - qv2^2 - qv3^2;
-o_dDCM(2,1) = 2*(qv1*qv2 - qv3*qs);
-o_dDCM(3,1) = 2*(qv1*qv3 + qv2*qs);
+dDCM(1,1) = qs^2 + qv1^2 - qv2^2 - qv3^2;
+dDCM(2,1) = 2*(qv1*qv2 - qv3*qs);
+dDCM(3,1) = 2*(qv1*qv3 + qv2*qs);
 
-o_dDCM(1,2) = 2*(qv1*qv2 + qv3*qs);
-o_dDCM(2,2) = qs^2 - qv1^2 + qv2^2 - qv3^2;
-o_dDCM(3,2) = 2*(qv2*qv3 - qv1*qs);
+dDCM(1,2) = 2*(qv1*qv2 + qv3*qs);
+dDCM(2,2) = qs^2 - qv1^2 + qv2^2 - qv3^2;
+dDCM(3,2) = 2*(qv2*qv3 - qv1*qs);
 
-o_dDCM(1,3) = 2*(qv1*qv3 - qv2*qs);
-o_dDCM(2,3) = 2*(qv2*qv3 + qv1*qs);
-o_dDCM(3,3) = qs^2 - qv1^2 - qv2^2 + qv3^2;
+dDCM(1,3) = 2*(qv1*qv3 - qv2*qs);
+dDCM(2,3) = 2*(qv2*qv3 + qv1*qs);
+dDCM(3,3) = qs^2 - qv1^2 - qv2^2 + qv3^2;
 
 end
