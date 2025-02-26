1a986c07ce94adb41a635a938176e21d1a116fcd
PeterC <petercalifano.gs@gmail.com>
2025-02-26 13:31:32 +0100

    Reorganize folders

--- matlab/linearAlgebra/givens_rotations/GivensEliminateRow.m
+++ matlab/linearAlgebra/givens_rotations/GivensEliminateRow.m
@@ -1,26 +1,26 @@
-function [dTargetMatrix] = GivensEliminateColumn(dTargetMatrix, ...
-                                                 ui32TargetSubscript, ...
-                                                 ui32AuxColId) %#codegen
+function [dTargetMatrix] = GivensEliminateRow(dTargetMatrix, ...
+                                              ui32TargetSubscript, ...
+                                              ui32AuxRowId) %#codegen
 arguments
     dTargetMatrix       (:,:) double
     ui32TargetSubscript (1,2) uint32    
-    ui32AuxColId        (1,1) uint32 = ui32TargetSubscript(2) - 1; 
+    ui32AuxRowId        (1,1) uint32 = ui32TargetSubscript(1) - 1; 
 end
 %% SIGNATURE
-% [dTargetMatrix] = GivensEliminateColumn(dTargetMatrix, ui32ColumnsIndices) %#codegen
+% [dTargetMatrix] = GivensEliminateRow(dTargetMatrix, ui32RowsIndices) %#codegen
 % -------------------------------------------------------------------------------------------------------------
 %% DESCRIPTION
 % Function performing elimination of a matrix entry identified by ui32TargetSubscript vector (row, col),
-% using Givens col-wise rotations (i.e., rotating two columns). 
+% using Givens row-wise rotations (i.e., rotating two rows). The algorithm can be used to perform QR
+% decomposition to find R in place.
 % -------------------------------------------------------------------------------------------------------------
 %% INPUT
 % dTargetMatrix       (:,:) double
-% ui32TargetSubscript (1,2) uint32 % In Golub, 2014, kth row
-% ui32AuxRowId        (1,1) uint32 = ui32TargetSubscript(1) - 1; % Default: element left target col, ith col
+% ui32TargetSubscript (1,2) uint32 % In Golub, 2014, ith row
+% ui32AuxRowId        (1,1) uint32 = ui32TargetSubscript(1) - 1; % Default: element above target row, kth row
 % -------------------------------------------------------------------------------------------------------------
 %% OUTPUT
-% % dTargetMatrix % Target matrix transformer in place
-
+% dTargetMatrix % Target matrix transformed in place
 % -------------------------------------------------------------------------------------------------------------
 %% CHANGELOG
 % 25-02-2025    Pietro Califano     First version implemented
@@ -32,33 +32,43 @@ end
 % [-]
 % -------------------------------------------------------------------------------------------------------------
 
-ui32NumOfRows = size(dTargetMatrix, 1);
+assert(ui32AuxRowId ~= ui32TargetSubscript(1)) 
+assert(ui32TargetSubscript(1) > 1, 'ERROR: Givens row-wise rotations cannot be applied to eliminate the 1st row. Target must be below the 1st pivot!')
+ui32NumOfCols = size(dTargetMatrix, 2);
 
 if coder.target("MATLAB") || coder.target("MEX") 
-
     % Assert sizes against indices
-    assert(ui32AuxColId ~= ui32TargetSubscript(2))
-    assert(ui32TargetSubscript(2) > 1, 'ERROR: Givens column-wise rotations cannot be applied to eliminate the 1st column. Target must be below the 1st pivot!')
-
+    % TODO
 end
 
 % Get (a,b) entries
-dVal1 = dTargetMatrix(ui32TargetSubscript(1),           ui32AuxColId);
+dVal1 = dTargetMatrix(ui32AuxRowId,           ui32TargetSubscript(2));
 dVal2 = dTargetMatrix(ui32TargetSubscript(1), ui32TargetSubscript(2));
 
 % Compute Givens rotation values
 [dCos, dSin] = GivensRotVals([dVal1; dVal2]);
 
 % Transform rows
-for ui32IdRow = 1:ui32NumOfRows
+for ui32IdCol = 1:ui32NumOfCols
     
-    dTmp1 = dTargetMatrix(ui32IdRow, ui32AuxColId); 
-    dTmp2 = dTargetMatrix(ui32IdRow, ui32TargetSubscript(2));
+    dTmp1 = dTargetMatrix(ui32AuxRowId, ui32IdCol); 
+    dTmp2 = dTargetMatrix(ui32TargetSubscript(1), ui32IdCol);
 
     % Rotate dTargetMatrix, equivalent to Arot = G^T * A
-    dTargetMatrix(ui32IdRow, ui32AuxColId) = dCos * dTmp1 - dSin * dTmp2;
-    dTargetMatrix(ui32IdRow, ui32TargetSubscript(2)) = dSin * dTmp1 + dCos * dTmp2;
+    dTargetMatrix(ui32AuxRowId, ui32IdCol) = dCos * dTmp1 - dSin * dTmp2;
+    dTargetMatrix(ui32TargetSubscript(1), ui32IdCol) = dSin * dTmp1 + dCos * dTmp2;
 
 end
 
 end
+
+
+
+
+
+
+
+
+
+
+
